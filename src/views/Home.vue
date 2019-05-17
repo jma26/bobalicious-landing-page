27974fb4e448ce514a4c8ee9b3c0c9878d97a60d
Jesse Ma <j.masonoma17@gmail.com>
2019-05-16 21:49:09 -0700

    Update styling for Navbar component, reorganized app component structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="home">
-    <Navbar />
-  </div>
+  <h1>Test</h1>
 </template>
 
 <script>
 // @ is an alias to /src
-import Navbar from '@/components/Navbar.vue';
 
 export default {
   name: 'home',
   components: {
-    Navbar
   },
 };
 </script>
