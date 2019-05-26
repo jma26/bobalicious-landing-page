d278b5aba9b6816dd2beb6739d9013dba5b62a18
Jesse Ma <j.masonoma17@gmail.com>
2019-05-25 23:08:19 -0700

    Add relative font-sizing for mobile devices at App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,36 @@ export default {
 
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  margin: 0;
-  padding: 0;
-  font-size: 100%;
-  vertical-align: baseline;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  // Relative font-sizing for mobile devices
+  body {
+    font-size: 12px !important;
+    margin: 0;
+    padding: 0;
+  }
+  h1 {
+    font-size: 240% !important;
+  }
+  h2 {
+    font-size: 220% !important;
+  }
+  h3 {
+    font-size: 200% !important;
+  }
+  h4 {
+    font-size: 180% !important;
+  }
+  h5 {
+    font-size: 160% !important;
+  }
+  h6 {
+    font-size: 140% !important;
+  }
+  p {
+    font-size: 120% !important;
+  }
 </style>
