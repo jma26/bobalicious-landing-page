0a6fdcb39364fd1345c169c15daa31b66df350c5
Jesse Ma <j.masonoma17@gmail.com>
2019-06-13 01:14:40 -0700

    Minor UI update and grid layout on Home component  for mobile views

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
-import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue';
+import App from './views/App.vue';
 import router from './router';
 import store from './store';
-import BootstrapVue from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import './registerServiceWorker';
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="Home">
     <div class="container">
-      <div class="row d-sm-block d-none">
-        <div class="col-12 Home__coming-soon-column">
-          <h2 class="Home__coming-soon-title">Coming soon...Bobalicious</h2>
+      <div class="row">
+        <div class="col-12 Home__company-name-column">
+          <h1 class="Home__company-name text-center">Bobalicious</h1>
         </div>
       </div>
       <div class="row">
         <div class="col-12 Home__call-to-action-column">
-          <h1 class="Home__call-to-action">Bobalicious is the new way to order your favorite bubble tea</h1>
+          <h1 class="Home__call-to-action">Open Source ordering system for small tea businesses</h1>
           <b-form>
-            <b-form-input v-model="text" class="Home__input--email" placeholder="Enter your email address"></b-form-input>
+            <b-form-input class="Home__input--email" placeholder="Enter your email address"></b-form-input>
             <b-button class="Home__submit-btn" type="submit">SIGN UP FOR BETA</b-button>
           </b-form>
           <p class="d-sm-block d-none Home__call-to-content">Waiting in long queues has never been easier! Order through this app and pick up your drink on arrival!</p>
@@ -90,14 +90,19 @@ export default {
     height: 100vh;
   }
 
-  .Home__call-to-action, .Home__coming-soon-title {
+  .Home__call-to-action, .Home__coming-soon-title, .Home__company-name {
     color: #FFF;
   }
 
   .Home__call-to-action {
     letter-spacing: 1.16px;
     text-align: center;
+    font-weight: 400;
+  }
+
+  .Home__company-name {
     font-weight: 700;
+    letter-spacing: 1.4px;
   }
 
   .Home__input--email {
